3d7f0c2860c7540831519eac1a8ca4b186b85daa
Mikhail Kovbasenko <hankchinaski34@gmail.com>
2021-05-26 15:23:05 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './AppContainer';
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
 	 <React.StrictMode>
       <AppContainer/>
     </React.StrictMode>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 reportWebVitals();
